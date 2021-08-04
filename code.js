38e593fd527c192bd3dca8be3735bc69a600efc5
Eggyeggman <88085440+Eggyeggman@users.noreply.github.com>
2021-08-04 19:39:58 +1000

    Add files via upload

---
+++ code.js
@@ -0,0 +1,141 @@
+var p5Inst = new p5(null, 'sketch');
+
+window.preload = function () {
+  initMobileControls(p5Inst);
+
+  p5Inst._predefinedSpriteAnimations = {};
+  p5Inst._pauseSpriteAnimationsByDefault = false;
+  var animationListJSON = {"orderedKeys":["26b80e63-bc0f-408d-b288-be2282aebd4e","eab56525-dbdc-402a-bc4d-35ff0e6d6f18","a31f35e3-97ec-41c0-8437-9abbae898aea","c26b6449-0b6c-4483-8486-107c0091c26e","3221caad-ae37-4eec-92a8-56bae4769e66","c4e310e0-8174-4127-a46e-0cbcc94b488b"],"propsByKey":{"26b80e63-bc0f-408d-b288-be2282aebd4e":{"name":"enemy1","sourceUrl":null,"frameSize":{"x":35,"y":50},"frameCount":1,"looping":true,"frameDelay":12,"version":"d2Z7UQrfXC21iUf5USi.seESbC6kA62n","categories":["icons"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":35,"y":50},"rootRelativePath":"assets/26b80e63-bc0f-408d-b288-be2282aebd4e.png"},"eab56525-dbdc-402a-bc4d-35ff0e6d6f18":{"name":"enemy3","sourceUrl":null,"frameSize":{"x":35,"y":50},"frameCount":1,"looping":true,"frameDelay":12,"version":"fou4TnEvxOAwtH_w.nPIxP3vQQqqAlz4","categories":["icons"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":35,"y":50},"rootRelativePath":"assets/eab56525-dbdc-402a-bc4d-35ff0e6d6f18.png"},"a31f35e3-97ec-41c0-8437-9abbae898aea":{"name":"enemy2","sourceUrl":null,"frameSize":{"x":35,"y":50},"frameCount":1,"looping":true,"frameDelay":12,"version":"KKHz4BU8iMFBCjhfFJ5l_iXx.PA8krCU","categories":["icons"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":35,"y":50},"rootRelativePath":"assets/a31f35e3-97ec-41c0-8437-9abbae898aea.png"},"c26b6449-0b6c-4483-8486-107c0091c26e":{"name":"hero1","sourceUrl":"assets/api/v1/animation-library/gamelab/loycQXdICpzI4PfXITdIndG9GcVBmRdK/category_faces/kidportrait_01.png","frameSize":{"x":264,"y":368},"frameCount":1,"looping":true,"frameDelay":2,"version":"loycQXdICpzI4PfXITdIndG9GcVBmRdK","categories":["faces"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":264,"y":368},"rootRelativePath":"assets/api/v1/animation-library/gamelab/loycQXdICpzI4PfXITdIndG9GcVBmRdK/category_faces/kidportrait_01.png"},"3221caad-ae37-4eec-92a8-56bae4769e66":{"name":"b","sourceUrl":"assets/api/v1/animation-library/gamelab/IJemJVUh3J1gcGlCdIJ8obCWhXAqxbUT/category_backgrounds/kitchen.png","frameSize":{"x":400,"y":400},"frameCount":1,"looping":true,"frameDelay":2,"version":"IJemJVUh3J1gcGlCdIJ8obCWhXAqxbUT","categories":["backgrounds"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":400,"y":400},"rootRelativePath":"assets/api/v1/animation-library/gamelab/IJemJVUh3J1gcGlCdIJ8obCWhXAqxbUT/category_backgrounds/kitchen.png"},"c4e310e0-8174-4127-a46e-0cbcc94b488b":{"name":"dream","sourceUrl":null,"frameSize":{"x":386,"y":268},"frameCount":1,"looping":true,"frameDelay":12,"version":"ZTILPpxHmV9JFwn3UCUCHUFAZDbboolF","categories":["icons"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":386,"y":268},"rootRelativePath":"assets/c4e310e0-8174-4127-a46e-0cbcc94b488b.png"}}};
+  var orderedKeys = animationListJSON.orderedKeys;
+  var allAnimationsSingleFrame = false;
+  orderedKeys.forEach(function (key) {
+    var props = animationListJSON.propsByKey[key];
+    var frameCount = allAnimationsSingleFrame ? 1 : props.frameCount;
+    var image = loadImage(props.rootRelativePath, function () {
+      var spriteSheet = loadSpriteSheet(
+          image,
+          props.frameSize.x,
+          props.frameSize.y,
+          frameCount
+      );
+      p5Inst._predefinedSpriteAnimations[props.name] = loadAnimation(spriteSheet);
+      p5Inst._predefinedSpriteAnimations[props.name].looping = props.looping;
+      p5Inst._predefinedSpriteAnimations[props.name].frameDelay = props.frameDelay;
+    });
+  });
+
+  function wrappedExportedCode(stage) {
+    if (stage === 'preload') {
+      if (setup !== window.setup) {
+        window.setup = setup;
+      } else {
+        return;
+      }
+    }
+// -----
+
+var b = createSprite(200,200);
+ b.setAnimation("b")
+var hero = createSprite(200,345,200,345)
+hero.shapeColor="red"
+
+var enemy1 = createSprite(200,250,10,10)
+enemy1.shapeColor="red"
+
+var enemy2 = createSprite(200,150,10,10)
+enemy2.shapeColor="red"
+
+var enemy3 = createSprite(200,50,10,10)
+enemy3.shapeColor="red"
+
+var net = createSprite(200,5,200,20)
+net.shapeColor="red"
+
+var goal =0;
+var death = 0
+
+hero.setAnimation("hero1");
+hero.scale=.2;
+enemy1.setAnimation("enemy1");
+
+enemy2.setAnimation("enemy2");
+
+enemy3.setAnimation("enemy3");
+
+
+enemy1.setVelocity(-10,0);
+enemy2.setVelocity(10,0);
+enemy3.setVelocity(-10,0);
+
+
+function draw() {
+  
+//background(b);
+
+createEdgeSprites()
+
+
+
+
+enemy1.bounceOff(edges)
+enemy2.bounceOff(edges)
+enemy3.bounceOff(edges)
+
+if(keyDown(UP_ARROW)){
+  hero.y=hero.y-3
+}
+
+if(keyDown(DOWN_ARROW)){
+  hero.y=hero.y+3
+}
+
+if(keyDown(LEFT_ARROW)){
+  hero.x=hero.x-3
+}
+
+if(keyDown(RIGHT_ARROW)){
+  hero.x=hero.x+3
+}
+
+if(hero.isTouching(enemy1)|| hero.isTouching(enemy2)|| hero.isTouching(enemy3)){
+  playSound("assets/category_achievements/bubbly_game_achievement_sound.mp3")
+  hero.x=200
+  hero.y=350
+  death = death+1
+}
+if(hero.isTouching(net)){
+  playSound("assets/category_achievements/vibrant_game_game_gold_tresure_chest_open.mp3")
+  hero.x=200
+  hero.y=345
+  goal=goal+1
+}
+textSize(20)
+  fill("blue")
+  text("Goals:"+goal,320,350);
+  
+
+textSize(20)
+  fill("blue")
+  text("death:"+death,20,350);
+  
+drawSprites()
+}
+
+// -----
+    try { window.draw = draw; } catch (e) {}
+    switch (stage) {
+      case 'preload':
+        if (preload !== window.preload) { preload(); }
+        break;
+      case 'setup':
+        if (setup !== window.setup) { setup(); }
+        break;
+    }
+  }
+  window.wrappedExportedCode = wrappedExportedCode;
+  wrappedExportedCode('preload');
+};
+
+window.setup = function () {
+  window.wrappedExportedCode('setup');
+};
